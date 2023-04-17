0d5c92617275be9aa12da3a99a57795ca6b5971e
Ethan R <89108631+SFgiantsfan@users.noreply.github.com>
2023-04-17 08:50:28 -0600

    Create Timer.java

---
+++ src/HackingFramework/Timer.java
@@ -0,0 +1,30 @@
+import java.time.LocalTime;
+
+public class Timer {
+
+	public void Start() {
+		LocalTime start = LocalTime.now();
+		System.out.println("Time starts now!");
+		//LocalTime next1 = start.plusMinutes(1);
+		//LocalTime next2 = next1.plusMinutes(1);
+		//LocalTime next3 = next2.plusMinutes(1);
+		//LocalTime next4 = next3.plusMinutes(1);
+		LocalTime next5 = start.plusMinutes(1);		
+		while(true) {
+//			if (LocalTime.now().compareTo(next1) ==0) {
+//			System.out.println("You have 4 minutes left.");
+//			} else if (LocalTime.now().compareTo(next2) ==0) {
+//				System.out.println("You have 3 minutes left.");
+//			}else if (LocalTime.now().compareTo(next3) ==0) {
+//				System.out.println("You have 2 minutes left.");
+//			}else if (LocalTime.now().compareTo(next4) ==0) {
+//				System.out.println("You have 1 minutes left.");
+/*			}else*/ if (LocalTime.now().compareTo(next5) ==0) {
+				System.out.println("Time is up!");
+				break;
+			}
+		}
+
+	}
+
+}
