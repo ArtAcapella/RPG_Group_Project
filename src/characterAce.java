e481810efe026620777c1fe7a7cdf721df02f30b
ArtAcapella <111773778+ArtAcapella@users.noreply.github.com>
2023-05-22 00:52:50 -0600

    Create characterAce.java

---
+++ src/characterAce.java
@@ -0,0 +1,28 @@
+public class characterAce
+{
+  protected int sts, bos, qck, buf, tech, eyes, cool, lck, chrm, ice, inl, hst, hck, corp, str, snk;
+  protected boolean laug, haug, remhk;
+  /*sts - street smarts; bos - book smarts; qck - quick; buf - buff; tech - tech; eyes - eyes; cool - cool; lck - luck; chrm - charm; ice - ICE; inl - intelligence; hst - history; hck - hack; corp - corporate; snk - sneak; laug - leg augment; haug - hand augment; remhk - remote hacking; str - strength*/
+  characterAce(int sts, int bos, int qck, int buf, int tech, int eyes, int cool, int lck, int chrm, int hst, int hck, int corp, int snk)
+  {
+    this.sts = sts;
+    this.bos = bos;
+    this.qck = qck;
+    this.buf = buf;
+    this.tech = tech;
+    this.eyes = eyes;
+    this.cool = cool;
+    this.lck = lck;
+    this.chrm = chrm;
+    this.ice = ice;
+    this.inl = inl;
+    this.hst = hst;
+    this.hck = hck;
+    this.corp = corp;
+    this.str = str;
+    this.snk = snk;
+    this.laug = laug;
+    this.haug = haug;
+    this.remhk = remhk;
+  }
+}
