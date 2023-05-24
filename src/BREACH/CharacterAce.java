459848fcc5fdfc3f44e5b961b81ffc2ae602f455
Ethan R <89108631+SFgiantsfan@users.noreply.github.com>
2023-05-24 07:35:17 -0600

    Create CharacterAce.java

---
+++ src/BREACH/CharacterAce.java
@@ -0,0 +1,28 @@
+public class characterAce
+{
+  protected int sts, bos, qck, buf, tech, eyes, cool, lck, chrm, ice, inl, hst, hck, corp, str, snk;
+  protected boolean laug, haug, remhk;
+  /*sts - street smarts; bos - book smarts; qck - quick; buf - buff; tech - tech; eyes - eyes; cool - cool; lck - luck; chrm - charm; ice - ICE; inl - intelligence; hst - history; hck - hack; corp - corporate; snk - sneak; laug - leg augment; haug - hand augment; remhk - remote hacking; str - strength*/
+  characterAce(int buf, int qck, int tech, int hck, int bos, int sts, int eyes, int cool, int chrm, int snk)
+  {
+    this.sts = sts;
+    this.bos = bos;
+    this.qck = qck;
+    this.buf = buf;
+    this.tech = tech;
+    this.eyes = eyes;
+    this.cool = cool;
+    this.lck = lck;
+    this.chrm = chrm;
+    this.ice = ice;
+    this.inl = inl;
+    this.hst = hst;
+    this.hck = hck;
+    this.corp = corp;
+    this.str = str;
+    this.snk = snk;
+    this.laug = laug;
+    this.haug = haug;
+    this.remhk = remhk;
+  }
+}
