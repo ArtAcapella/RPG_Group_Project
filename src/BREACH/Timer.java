2fa20fae5bfb6a2ea7870578cb2937a1498a7ba3
Sun-Punks <89051321+Sun-Punks@users.noreply.github.com>
2023-05-26 08:53:09 -0600

    Update Timer.java

--- src/BREACH/Timer.java
+++ src/BREACH/Timer.java
@@ -12,21 +12,21 @@ public class Timer {
 		LocalTime next3 = next2.plusSeconds(seconds);
 		LocalTime next4 = next3.plusSeconds(seconds);
 		LocalTime next5 = next4.plusSeconds(seconds);
-		while(true) {
-			if (LocalTime.now().compareTo(next1) ==0) {
-			System.out.println("You have" + seconds*4 + "seconds left.");
-			} else if (LocalTime.now().compareTo(next2) ==0) {
-				System.out.println("You have" + seconds*3 + "seconds left.");
-			}else if (LocalTime.now().compareTo(next3) ==0) {
-				System.out.println("You have" + seconds*2 + "seconds left.");
-			}else if (LocalTime.now().compareTo(next4) ==0) {
+		while (true) {
+			if (LocalTime.now().compareTo(next1) == 0) {
+				System.out.println("You have" + seconds * 4 + "seconds left.");
+			} else if (LocalTime.now().compareTo(next2) == 0) {
+				System.out.println("You have" + seconds * 3 + "seconds left.");
+			} else if (LocalTime.now().compareTo(next3) == 0) {
+				System.out.println("You have" + seconds * 2 + "seconds left.");
+			} else if (LocalTime.now().compareTo(next4) == 0) {
 				System.out.println("You have" + seconds + "seconds left.");
-			}else if (LocalTime.now().compareTo(start) ==0) {
+			} else if (LocalTime.now().compareTo(start) == 0) {
 				System.out.println("Time is up!");
 				break;
 			}
 		}
 
 	}
-	
+
 }
